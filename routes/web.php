bf7a20de3127bac95c3ea9da5d30ab3853a4502c
Alexey <alexey.cherevuta@devit-team.com>
2021-01-17 10:51:09 +0000

    Initial routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/configurations', 'ConfigurationController@index')->name('configurations.index');
+Route::get('/configurations/create', 'ConfigurationController@create')->name('configurations.create');
+Route::get('/configurations/{id}/edit/{key}', 'ConfigurationController@update')->name('configurations.update');
+Route::post('/configurations/store', 'ConfigurationController@store')->name('configurations.store');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');

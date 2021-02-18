5800aa8c0b14eaf11f2a7a58937ad11b6125835a
Alexey <alexey.cherevuta@devit-team.com>
2021-02-18 13:58:23 +0000

    Added accordion to filter form

--- resources/views/configurations/index.blade.php
+++ resources/views/configurations/index.blade.php
@@ -5,112 +5,180 @@
         <div class="col-12">
             <form id="filterConfigurationsForm" method="get" action="{{ route('configurations.index') }}">
                 @csrf
-                <div class="form-row">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="form-group">
-                            <label class="text-capitalize">Device type</label>
-                            <select id="device_type" class="form-control" name="device_type">
-                                <option value="" {{ request()->device_type ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['device_types'] as $device_type)
-                                <option value="{{ $device_type->name }}"
-                                    {{ request()->device_type == $device_type->name ? 'selected' : '' }}>
-                                    {{ $device_type->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                <div class="accordion my-3" id="accordionFilter">
+                    <div class="card">
+                        <div class="card-header py-0" id="headingDevice">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse"
+                                    data-target="#deviceCollapse" aria-expanded="true" aria-controls="deviceCollapse">
+                                    Device
+                                </button>
+                            </h2>
                         </div>
-                        <div class="form-group">
-                            <label for="device_manufacturer" class="text-capitalize">Manufacturer</label>
-                            <select id="device_manufacturer" class="form-control" name="device_manufacturer">
-                                <option value="" {{ request()->device_manufacturer ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['device_manufacturers'] as $device_manufacturer)
-                                <option value="{{ $device_manufacturer->name }}"
-                                    {{ request()->device_manufacturer == $device_manufacturer->name ? 'selected' : '' }}>
-                                    {{ $device_manufacturer->name }}
-                                </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="device_model" class="text-capitalize">Device model</label>
-                            <select id="device_model" class="form-control" name="device_model">
-                                <option value="" {{ request()->device_model ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['device_models'] as $device_model)
-                                <option value="{{ $device_model->name }}"
-                                    {{ request()->device_model == $device_model->name ? 'selected' : '' }}>
-                                    {{ $device_model->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                        <div id="deviceCollapse" class="collapse show" aria-labelledby="headingDevice"
+                            data-parent="#accordionFilter">
+                            <div class="card-body">
+                                <div class="form-row">
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label class="text-capitalize">Type</label>
+                                            <select id="device_type" class="form-control" name="device_type">
+                                                <option value="" {{ request()->device_type ? '' : 'selected' }}>-
+                                                </option>
+                                                @foreach($distinctValues['device_types'] as $device_type)
+                                                <option value="{{ $device_type->name }}"
+                                                    {{ request()->device_type == $device_type->name ? 'selected' : '' }}>
+                                                    {{ $device_type->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="device_manufacturer"
+                                                class="text-capitalize">Manufacturer</label>
+                                            <select id="device_manufacturer" class="form-control"
+                                                name="device_manufacturer">
+                                                <option value="" {{ request()->device_manufacturer ? '' : 'selected' }}>
+                                                    -
+                                                </option>
+                                                @foreach($distinctValues['device_manufacturers'] as
+                                                $device_manufacturer)
+                                                <option value="{{ $device_manufacturer->name }}"
+                                                    {{ request()->device_manufacturer == $device_manufacturer->name ? 'selected' : '' }}>
+                                                    {{ $device_manufacturer->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="device_model" class="text-capitalize">Model</label>
+                                            <select id="device_model" class="form-control" name="device_model">
+                                                <option value="" {{ request()->device_model ? '' : 'selected' }}>-
+                                                </option>
+                                                @foreach($distinctValues['device_models'] as $device_model)
+                                                <option value="{{ $device_model->name }}"
+                                                    {{ request()->device_model == $device_model->name ? 'selected' : '' }}>
+                                                    {{ $device_model->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="form-group">
-                            <label for="cpu_manufacturer" class="text-capitalize">CPU manufacturer</label>
-                            <select id="cpu_manufacturer" class="form-control" name="cpu_manufacturer">
-                                <option value="" {{ request()->cpu_manufacturer ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['cpu_manufacturers'] as $cpu_manufacturer)
-                                <option value="{{ $cpu_manufacturer->name }}"
-                                    {{ request()->cpu_manufacturer == $cpu_manufacturer->name ? 'selected' : '' }}>
-                                    {{ $cpu_manufacturer->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                    <div class="card">
+                        <div class="card-header py-0" id="headingCpu">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                    data-target="#cpuCollapse" aria-expanded="false" aria-controls="cpuCollapse">
+                                    CPU
+                                </button>
+                            </h2>
                         </div>
-                        <div class="form-group">
-                            <label for="cpu_model" class="text-capitalize">CPU model</label>
-                            <select id="cpu_model" class="form-control" name="cpu_model">
-                                <option value="" {{ request()->cpu_model ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['cpu_models'] as $cpu_model)
-                                <option value="{{ $cpu_model->name }}"
-                                    {{ request()->cpu_model == $cpu_model->name ? 'selected' : '' }}>
-                                    {{ $cpu_model->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                        <div id="cpuCollapse" class="collapse show" aria-labelledby="headingCpu"
+                            data-parent="#accordionFilter">
+                            <div class="card-body">
+                                <div class="form-row">
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="cpu_manufacturer" class="text-capitalize">Manufacturer</label>
+                                            <select id="cpu_manufacturer" class="form-control" name="cpu_manufacturer">
+                                                <option value="" {{ request()->cpu_manufacturer ? '' : 'selected' }}>-
+                                                </option>
+                                                @foreach($distinctValues['cpu_manufacturers'] as $cpu_manufacturer)
+                                                <option value="{{ $cpu_manufacturer->name }}"
+                                                    {{ request()->cpu_manufacturer == $cpu_manufacturer->name ? 'selected' : '' }}>
+                                                    {{ $cpu_manufacturer->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="cpu_model" class="text-capitalize">Model</label>
+                                            <select id="cpu_model" class="form-control" name="cpu_model">
+                                                <option value="" {{ request()->cpu_model ? '' : 'selected' }}>-</option>
+                                                @foreach($distinctValues['cpu_models'] as $cpu_model)
+                                                <option value="{{ $cpu_model->name }}"
+                                                    {{ request()->cpu_model == $cpu_model->name ? 'selected' : '' }}>
+                                                    {{ $cpu_model->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="form-group">
-                            <label for="distribution" class="text-capitalize">Distribution</label>
-                            <select id="distribution" class="form-control" name="distribution">
-                                <option value="" {{ request()->distribution ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['distributions'] as $distribution)
-                                <option value="{{ $distribution->name }}"
-                                    {{ request()->distribution == $distribution->name ? 'selected' : '' }}>
-                                    {{ $distribution->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                    <div class="card">
+                        <div class="card-header py-0" id="headingLinux">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                    data-target="#collapseLinux" aria-expanded="false" aria-controls="collapseLinux">
+                                    Linux
+                                </button>
+                            </h2>
                         </div>
-                        <div class="form-group">
-                            <label for="kernel" class="text-capitalize">Kernel</label>
-                            <select id="kernel" class="form-control" name="kernel">
-                                <option value="" {{ request()->kernel ? '' : 'selected' }}>-</option>
-                                @foreach($distinctValues['kernels'] as $kernel)
-                                <option value="{{ $kernel->name }}"
-                                    {{ request()->kernel == $kernel->name ? 'selected' : '' }}>
-                                    {{ $kernel->name }}
-                                </option>
-                                @endforeach
-                            </select>
+                        <div id="collapseLinux" class="collapse show" aria-labelledby="headingLinux"
+                            data-parent="#accordionFilter">
+                            <div class="card-body">
+                                <div class="form-row">
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="distribution" class="text-capitalize">Distribution</label>
+                                            <select id="distribution" class="form-control" name="distribution">
+                                                <option value="" {{ request()->distribution ? '' : 'selected' }}>-
+                                                </option>
+                                                @foreach($distinctValues['distributions'] as $distribution)
+                                                <option value="{{ $distribution->name }}"
+                                                    {{ request()->distribution == $distribution->name ? 'selected' : '' }}>
+                                                    {{ $distribution->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group m-0">
+                                            <label for="kernel" class="text-capitalize">Kernel</label>
+                                            <select id="kernel" class="form-control" name="kernel">
+                                                <option value="" {{ request()->kernel ? '' : 'selected' }}>-</option>
+                                                @foreach($distinctValues['kernels'] as $kernel)
+                                                <option value="{{ $kernel->name }}"
+                                                    {{ request()->kernel == $kernel->name ? 'selected' : '' }}>
+                                                    {{ $kernel->name }}
+                                                </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-12">
                         <div class="form-group d-flex justify-content-end">
-                            <button type="submit" class="btn btn-primary text-uppercase font-weight-bold">
-                                <i class="fas fa-search mr-2"></i>Search
-                            </button>
-                            <a href="{{ route('configurations.index') }}"
-                                class="btn btn-warning text-uppercase font-weight-bold ml-0 mt-3 ml-sm-2 mt-sm-0">
-                                <i class="fas fa-trash mr-2"></i>Clear
-                            </a>
                             <a href="{{ route('configurations.create') }}"
-                                class="btn btn-success text-uppercase font-weight-bold ml-0 mt-3 ml-sm-2 mt-sm-0">
+                                class="btn btn-success text-uppercase font-weight-bold">
                                 <i class="fas fa-plus mr-2"></i>Add
                             </a>
+                            <a href="{{ route('configurations.index') }}"
+                                class="btn btn-warning text-uppercase font-weight-bold ml-2">
+                                <i class="fas fa-trash mr-2"></i>Clear
+                            </a>
+                            <button type="submit" class="btn btn-primary text-uppercase font-weight-bold ml-2">
+                                <i class="fas fa-search mr-2"></i>Search
+                            </button>
                         </div>
                     </div>
                 </div>

a58b9c372c2e6d06fe64c8ca497aac8cc456c9d1
Alexey <alexey.cherevuta@devit-team.com>
2021-04-10 17:45:10 +0000

    Edit db:dump command

--- app/Console/Commands/DatabaseDump.php
+++ app/Console/Commands/DatabaseDump.php
@@ -5,7 +5,8 @@ use App\Configuration;
 use Illuminate\Console\Command;
 class DatabaseDump extends Command
 {
-    protected $signature = 'db:dump {--private : Dump Configurations table with keys}';
+    protected $signature = 'db:dump {--private : Dump Configurations table with keys}
+                                    {--csv : Export as .csv (default: .json)}';
     protected $description = 'Dump Configurations table to ./database/data/dumps';
     public function __construct()
     {
@@ -14,18 +15,27 @@ class DatabaseDump extends Command
     public function handle()
     {
         $date = Carbon::now()->format('Y-m-d-His');
+        $this->option('csv') ? $format = '.csv' : $format = '.json';
         if ($this->option('private')) {
-            if (!file_exists('./database/data/dumps/private')){
-                mkdir("./database/data/dumps/private", 0700);
+            if (!file_exists('./database/data/dumps/private')) {
+                mkdir('./database/data/dumps/private', 0700);
             }
             $configurations = Configuration::all()->makeVisible('key')->toJson();
-            $file = fopen('./database/data/dumps/private/'.$date.'_dump-private.json', 'w');
-        } else{
+            $file = fopen('./database/data/dumps/private/'.$date.'_dump-private'.$format, 'w');
+        } else {
             $configurations = Configuration::all()->makeHidden('id')->toJson();
-            $file = fopen('./database/data/dumps/'.$date.'_dump.json', 'w');
+            $file = fopen('./database/data/dumps/'.$date.'_dump'.$format, 'w');
         }
-        fwrite($file, $configurations);
-        fclose($file);
+        if ($this->option('csv')) {
+            $decodedJson = json_decode($configurations, true);
+            foreach ($decodedJson as $configuration) {
+                fputcsv($file, $configuration);
+            }
+            fclose($file);
+        } else {
+            fwrite($file, $configurations);
+            fclose($file);
+        }
         return true;
     }
 }

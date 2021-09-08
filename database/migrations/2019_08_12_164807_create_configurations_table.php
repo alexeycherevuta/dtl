32b2df0e529343e961c6c942ef8fc33a69d0d82d
Alexey <alexey.cherevuta@devit-team.com>
2021-09-08 05:36:45 +0000

    Edit migration

--- database/migrations/2019_08_12_164807_create_configurations_table.php
+++ database/migrations/2019_08_12_164807_create_configurations_table.php
@@ -9,15 +9,25 @@ class CreateConfigurationsTable extends Migration
         Schema::create('configurations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('device_type')->nullable()->default(null);
+            $table->index('device_type');
             $table->string('device_manufacturer')->nullable()->default(null);
+            $table->index('device_manufacturer');
             $table->string('device_model')->nullable()->default(null);
+            $table->index('device_model');
             $table->string('cpu_manufacturer')->nullable()->default(null);
+            $table->index('cpu_manufacturer');
             $table->string('cpu_model')->nullable()->default(null);
+            $table->index('cpu_model');
             $table->string('gpu_manufacturer')->nullable()->default(null);
+            $table->index('gpu_manufacturer');
             $table->string('gpu_model')->nullable()->default(null);
+            $table->index('gpu_model');
             $table->string('gpu_driver')->nullable()->default(null);
+            $table->index('gpu_driver');
             $table->string('distribution')->nullable()->default(null);
+            $table->index('distribution');
             $table->string('kernel')->nullable()->default(null);
+            $table->index('kernel');
             $table->mediumText('comment')->nullable()->default(null);
             $table->string('key')->nullable()->default(null);
             $table->timestamps();
